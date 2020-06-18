33e227e76af2bcf969fa389b067073d699c73c8e
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-06-18 20:21:52 +0200

    ProvidersBase RavenDB Orleans Error Codes

---
+++ src/orleans-ravendb/Orleans.Persistence.RavenDB/Provider/RavenDBProviderErrorCode.cs
@@ -0,0 +1,20 @@
+﻿namespace Orleans.Persistence.RavenDB.Provider
+{
+    internal enum RavenDBProviderErrorCode
+    {
+        ProvidersBase = 200000,
+
+        // RavenDB storage provider related
+        RavenDBProviderBase = ProvidersBase + 1100,
+        RavenDBProvider_DataNotFound = RavenDBProviderBase + 1,
+        RavenDBProvider_ReadingData = RavenDBProviderBase + 2,
+        RavenDBProvider_WritingData = RavenDBProviderBase + 3,
+        RavenDBProvider_Storage_Reading = RavenDBProviderBase + 4,
+        RavenDBProvider_Storage_Writing = RavenDBProviderBase + 5,
+        RavenDBProvider_Storage_DataRead = RavenDBProviderBase + 6,
+        RavenDBProvider_WriteError = RavenDBProviderBase + 7,
+        RavenDBProvider_DeleteError = RavenDBProviderBase + 8,
+        RavenDBProvider_InitProvider = RavenDBProviderBase + 9,
+        RavenDBProvider_ParamConnectionString = RavenDBProviderBase + 10,
+    }
+}
