f1ab6d336801ea4715bb34b0d5c5fe5f79608a0e
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-06-18 19:21:16 +0200

    unstable: adding test harness with ravendb storage provider

---
+++ src/orleans-ravendb/Orleans.Persistence.RavenDB.Tests/TestSiloConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using Orleans.Hosting;
+using Orleans.TestingHost;
+
+namespace Orleans.Persistence.RavenDB.Tests
+{
+    public class TestSiloConfigurator : ISiloConfigurator
+    {
+        public void Configure(ISiloBuilder siloBuilder)
+        {
+            siloBuilder
+                .AddRavenDBGrainStorage(name: "unit-test");
+        }
+    }
+}
