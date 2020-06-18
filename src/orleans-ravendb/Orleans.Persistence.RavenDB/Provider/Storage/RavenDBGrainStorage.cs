e285d30feca65933a12d781e80fca82984d028a9
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-06-18 20:21:33 +0200

    unstable: partial implementation of Silo Life Cycle Participation

--- src/orleans-ravendb/Orleans.Persistence.RavenDB/Provider/Storage/RavenDBGrainStorage.cs
+++ src/orleans-ravendb/Orleans.Persistence.RavenDB/Provider/Storage/RavenDBGrainStorage.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Orleans.Configuration;
+using Orleans.Persistence.RavenDB.Provider;
 using Orleans.Runtime;
+using Orleans.Runtime.Configuration;
 using Orleans.Serialization;
 using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Orleans.Storage
@@ -20,6 +25,7 @@ namespace Orleans.Storage
         private readonly IGrainFactory grainFactory;
         private readonly ITypeResolver typeResolver;
         private readonly ILogger<RavenDBGrainStorage> logger;
+        private JsonSerializerSettings jsonSettings;
 
         public RavenDBGrainStorage(
         string name,
@@ -44,7 +50,7 @@ namespace Orleans.Storage
 
         public void Participate(ISiloLifecycle lifecycle)
         {
-            throw new NotImplementedException();
+            lifecycle.Subscribe(OptionFormattingUtilities.Name<RavenDBGrainStorage>(this.name), this.options.InitStage, Init);
         }
 
         public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
@@ -55,6 +61,37 @@ namespace Orleans.Storage
         public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary> Initialization function for this storage provider. </summary>
+        private async Task Init(CancellationToken ct)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                this.logger.LogInformation((int)RavenDBProviderErrorCode.RavenDBProvider_InitProvider, $"AzureTableGrainStorage initializing: {this.options.ToString()}");
+                this.logger.LogInformation((int)RavenDBProviderErrorCode.RavenDBProvider_ParamConnectionString, "AzureTableGrainStorage is using DataConnectionString: {0}", ConfigUtilities.RedactConnectionStringInfo(this.options.ConnectionString));
+                this.jsonSettings = OrleansJsonSerializer.UpdateSerializerSettings(OrleansJsonSerializer.GetDefaultSerializerSettings(this.typeResolver, this.grainFactory), this.options.UseFullAssemblyNames, this.options.IndentJson, this.options.TypeNameHandling);
+
+                this.options.ConfigureJsonSerializerSettings?.Invoke(this.jsonSettings);
+
+                // TODO: RavenDB Specific Code here.
+                /*
+                var account = CloudStorageAccount.Parse(this.options.ConnectionString);
+                var blobClient = account.CreateCloudBlobClient();
+                container = blobClient.GetContainerReference(this.options.ContainerName);
+                await container.CreateIfNotExistsAsync().ConfigureAwait(false);
+                */
+                stopWatch.Stop();
+                this.logger.LogInformation((int)RavenDBProviderErrorCode.RavenDBProvider_InitProvider, $"Initializing provider {this.name} of type {this.GetType().Name} in stage {this.options.InitStage} took {stopWatch.ElapsedMilliseconds} Milliseconds.");
+            }
+            catch (Exception ex)
+            {
+                stopWatch.Stop();
+                this.logger.LogError((int)ErrorCode.Provider_ErrorFromInit, $"Initialization failed for provider {this.name} of type {this.GetType().Name} in stage {this.options.InitStage} in {stopWatch.ElapsedMilliseconds} Milliseconds.", ex);
+                throw;
+            }
         }
     }
 }
